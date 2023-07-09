3a01370df23476d3309733ac9c52a2e205b2fc4e
ArtemAirapetov <as.airapetov@gmail.com>
2023-07-09 20:06:12 +0500

    Поменяли имя

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        String name = ("artyom");
+        String name = ("Вася");
         System.out.println(name);
     }
 
